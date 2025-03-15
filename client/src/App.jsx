3f4f1c0eacea97415a93019883e975c996e09748
emilabos <138469562+emilabos@users.noreply.github.com>
2025-03-15 16:17:02 +0000

    fix1231312

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import useWebSocket, { ReadyState } from "react-use-websocket";
 import Cookies from "js-cookie";
 import AdPopup from "./components/Popup";
 
-const WS_URL = "ws://localhost:8000/ws/";
-const API_URL = "http://localhost:8000";
+const WS_URL = "ws://scrappy-chat.onrender.com/ws/";
+const API_URL = "https://scrappy-chat.onrender.com";
 
 const UsernameModal = ({ setUserName, visible, setShowModal }) => {
   const [nameInput, setNameInput] = useState("");
